fa48c829b73c15f445fc7635bacab4cc3ad6a4d4
sriramede9 <only4programming03@gmail.com>
2019-12-13 12:55:42 -0500

    added status code

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 var express = require("express");
 var app = express();
 
+const customers = [
+  { id: 1, name: "Sri" },
+  { id: 2, name: "Sri Ram" },
+  { id: 3, name: "Sri Ede" }
+];
+
 app.get("/", (req, res) => {
   res.send("this is test");
 });
 
 app.get("/api/customers", function(req, res) {
-  res.send([1, 2, 3]);
+  res.send(customers);
 });
 
-app.get("/customer/:id", function(req, res) {
-  res.send("Customer " + req.params.id);
+app.get("/api/customer/:id", function(req, res) {
+  const customer = customers.find(c => c.id === parseInt(req.params.id));
+
+  if (!customer) {
+    res.status(404).send("the customer requested in no longer available");
+  }
+
+  res.send(customer);
 });
 
 const port = process.env.PORT || 3300;
