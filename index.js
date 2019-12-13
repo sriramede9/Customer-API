c457c5c0bc75984e4d06bb2b835039a57c4f9d55
sriramede9 <only4programming03@gmail.com>
2019-12-13 12:08:44 -0500

    added env variable port and cmd SET PORT=9000 && node index.js on windows

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ app.get("/", (req, res) => {
   res.send("this is test");
 });
 
-app.listen(3300);
+app.get("/api/customers", function(req, res) {
+  res.send([1, 2, 3]);
+});
+
+app.get("/customer/:id", function(req, res) {
+  res.send("Customer " + req.params.id);
+});
+
+const port = process.env.PORT || 3300;
+
+app.listen(port, () => console.log(`listening on port ${port} ...`));
