ff2b82da67f360bb0e4381982814a28fc9c8d1df
sriramede9 <only4programming03@gmail.com>
2019-12-13 13:44:27 -0500

    added validation Joi along with Post operation and app.use(express.json)

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+const Joi = require("joi");
+
+//const joi = new Joi();
+
+app.use(express.json());
+
 const customers = [
   { id: 1, name: "Sri" },
   { id: 2, name: "Sri Ram" },
@@ -23,6 +29,34 @@ app.get("/api/customer/:id", function(req, res) {
   }
 
   res.send(customer);
+});
+
+//handle post request
+
+app.post("/api/customers", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const customer = {
+    id: customers.length + 1,
+    name: req.body.name
+  };
+
+  //validate the input before you push
+
+  customers.push(customer);
+
+  res.status(200).send(customer);
 });
 
 const port = process.env.PORT || 3300;
